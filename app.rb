d009421eb9e2d8b0db59b49b1245c5dcaf0247a4
yellowpad <zayneabraham1@gmail.com>
2017-02-21 06:32:04 -0500

    Sinatra routes

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/' do
+		
+	end
+
+	get '/name' do
+		"My name is Zayne"
+	end
+
+	get '/hometown' do 
+		"My hometown is Jersey City, Alabansarkansa"
+	end
+
+	get '/favorite-song' do 
+		"My favorite song is if I could turn back time by Ennio Morricone"
+	end
+
 end
